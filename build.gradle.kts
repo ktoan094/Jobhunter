aaf41b22445aff1c124af27d38665385f0fbecca
ToanNguyen <toantomtich@gmail.com>
2025-01-14 21:40:02 +0700

    Video 70

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,10 @@ plugins {
 	java
 	id("org.springframework.boot") version "3.2.4"
 	id("io.spring.dependency-management") version "1.1.4"
+	plugins {
+  		id("io.freefair.lombok") version "8.6"
+	}
+
 }
 
 group = "vn.hoidanit"
